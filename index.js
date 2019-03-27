03d0db324559288b5664581d6475934e9d0ca32d
Tatiana Haddad <tatianadhaddad@gmail.com>
2019-03-26 20:42:48 -0700

    added results list

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const apiKey = 'zmmW3cBfAyPh0fqBsG3t9ISwkndGYDtwhj5I0kUC'
 
+const searchURL = 'https://developer.nps.gov/api/v1/parks'
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson, maxResults) {
+  console.log("working", responseJson);
+  $('#results-list').empty();
+  // iterate through the articles array, stopping at the max number of results
+  const parks = responseJson.map(function(park) {
+    return `<li><h3>${park.fullName}</h3></li>
+    <li>${park.description}<li>
+    <li><a href="${park.url}">${park.url}</a></li>`;
+  });
+  console.log('it works', parks);
+  $('#results-list').append(parks) 
+  $('#results').removeClass('hidden');
+};
+
 function getParks(query, maxResults=5) {
   const params = {
-    stateCode: query,
+    parkCode: query,
     limit: maxResults,
     language: "en",
   };
@@ -10,26 +32,30 @@ function getParks(query, maxResults=5) {
   const url = searchURL + '?' + queryString;
 
   console.log(url);
-  const url = `https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${apiKey}`;
+
+  const options = {
+    headers: new Headers({
+      "X-Api-Key": apiKey})
+  };
 
   fetch(url)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson, maxResults))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
-
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${key}=${params[key]}`)
-  return queryItems.join('&');
-}
-
-
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     const searchTerm = $('#js-search-term').val();
     const maxResults = $('#js-max-results').val();
-    getNews(searchTerm, maxResults);
+    getParks(searchTerm, maxResults);
   });
 }
 
